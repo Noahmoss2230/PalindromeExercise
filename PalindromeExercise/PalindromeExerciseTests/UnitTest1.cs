2eb4e218d02a2d384264ac33d5ac48f7e9ef0d92
Noah Moss <noahmoss50@gmail.com>
2025-06-23 14:20:39 -0500

    added more InLineData

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -9,15 +9,19 @@ namespace PalindromeExerciseTests
         [Theory]
         [InlineData("racecar", true)]
         [InlineData("hello", false)]
+        [InlineData("hannah", true)]
+        [InlineData("smash", false)]
+        [InlineData("pop", true)]
+        [InlineData("tree", false)]
+        [InlineData("rock", false)]
+        [InlineData("fish", false)]
+        [InlineData("kayak", true)]
+        [InlineData("level", true)]
         public void MyTest(string word, bool expected)
         {
             var tester = new WordSmith();
             var actual = tester.IsAPalindrome(word);
             Assert.Equal(expected, actual);
-        }
-        public void Test1()
-        {
-
         }
     }
 }

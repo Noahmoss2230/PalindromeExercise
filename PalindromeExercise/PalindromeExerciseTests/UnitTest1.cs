0d2eefe2e95825028801ca0c1faff56c3ae85d17
Noah Moss <noahmoss50@gmail.com>
2025-06-23 13:53:49 -0500

    Finished Paindrome Exercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,11 +1,20 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void MyTest(string word, bool expected)
+        {
+            var tester = new WordSmith();
+            var actual = tester.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
+        }
         public void Test1()
         {
 
